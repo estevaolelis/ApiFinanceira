c76981afdae17b5c61478db3b237eb3e64daf15d
estevaolelis <estevaolelis@gmail.com>
2025-12-03 17:10:35 -0300

    fix: outro ajuste

---
+++ ApiFinanceira/Program.cs
@@ -0,0 +1,37 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile("appsettings.Development.json", optional: true, reloadOnChange: true)
+    .AddEnvironmentVariables();
+
+builder.Services.AddSingleton(provider =>
+{
+    var config = builder.Configuration.GetSection("Supabase");
+    var url = config["Url"];
+    var key = config["ServiceKey"];
+
+    return SupabaseConfig.Initialize(url, key);
+});
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
